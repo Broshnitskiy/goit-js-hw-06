c20547c4b99f711f5c46d263186d3534a87e6e69
Roman Broshnitskiy <romario777v8@gmail.com>
2021-10-12 22:53:20 +0300

    вирішує task_2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsRef = document.querySelector("#ingredients");
+
+ingredients.forEach(ingredient => {
+  const item = document.createElement("li");
+  item.textContent = ingredient;
+  item.classList.add("item");
+
+  ingredientsRef.append(item);
+})
+
+console.log(ingredientsRef);
