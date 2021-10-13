08ab62d61fb875269c004b639dae2b69b7bd5550
Roman Broshnitskiy <romario777v8@gmail.com>
2021-10-13 23:41:47 +0300

    Рішає Task-6

---
+++ js/task-06.js
@@ -0,0 +1,30 @@
+const inputValidationRef = document.querySelector("#validation-input");
+
+function handleInputBlur (event) {
+ 
+    if(event.currentTarget.value.length === 6) {
+        inputValidationRef.classList.add("valid")
+        inputValidationRef.classList.remove("invalid")
+    } else {
+        inputValidationRef.classList.add("invalid")
+        inputValidationRef.classList.remove("valid")
+    }
+}
+ 
+inputValidationRef.addEventListener("blur", handleInputBlur)
+
+
+// inputValidationRef.addEventListener("blur", event => {
+ 
+//     if(event.currentTarget.value.length === 6) {
+//         inputValidationRef.classList.add("valid")
+//         inputValidationRef.classList.remove("invalid")
+//     } else {
+//         inputValidationRef.classList.add("invalid")
+//         inputValidationRef.classList.remove("valid")
+//     }
+// })
+ 
+
+
+ 
