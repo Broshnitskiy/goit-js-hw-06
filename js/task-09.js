346e120aec40a95cdc20297d3db5e0f14c84b166
Roman Broshnitskiy <romario777v8@gmail.com>
2021-10-14 22:38:58 +0300

    скрипт task-9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyRef = document.querySelector("body");
+const colorRef = document.querySelector(".color");
+const buttonRef = document.querySelector(".change-color");
+
+buttonRef.addEventListener("click", () => {
+  const randomColor = getRandomHexColor();
+  bodyRef.style.backgroundColor = `${randomColor}`;
+  colorRef.textContent = `${randomColor}`;
+})
