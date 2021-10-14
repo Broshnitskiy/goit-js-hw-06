2b533b610205c5cfb2a44d1aeb0c118d80073ee6
Roman Broshnitskiy <romario777v8@gmail.com>
2021-10-14 22:22:54 +0300

    скрипт task-8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const loginFormRef = document.querySelector(".login-form");
 
+const handlerLoginSubmit = event => {
+    event.preventDefault();
+    const {elements: { email, password }} = event.currentTarget;
+
+    if (email.value === "" || password.value === "") {
+    return alert("Уcі поля повинні бути заповнені!");
+    }
+
+    console.log({
+        email: email.value,
+        password: password.value
+    });
+    
+    event.currentTarget.reset();
+ }
+
+loginFormRef.addEventListener("submit", handlerLoginSubmit);
