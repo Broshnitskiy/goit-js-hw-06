ece6a7f85954f9acf87d8e28b404476932e03506
Roman Broshnitskiy <romario777v8@gmail.com>
2021-10-15 22:06:35 +0300

    task-10 скритп

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,37 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
+const inputRef = document.querySelector("input");
+const buttonCreateRef = document.querySelector("[data-create]");
+const buttonDestroyRef = document.querySelector("[data-destroy]");
+const boxesRef = document.querySelector("#boxes");
+
+
+
+function createBoxes(amount) {
+  let boxSize = 20;
+  for (let i = 1; i <= amount; i += 1) {
+    
+    boxSize += 10;
+    const markup = `<div style="width: ${boxSize}px; height: ${boxSize}px; background-color:${getRandomHexColor()};"></div>`
+    boxesRef.insertAdjacentHTML("beforeend", markup);
+  }
+};
+
+function destroyBoxes() {
+  boxesRef.innerHTML = '';
+};
+
+buttonCreateRef.addEventListener("click", () => {
+  createBoxes(inputRef.value);
+});
+
+
+buttonDestroyRef.addEventListener("click", () => {
+   destroyBoxes()
+});
+
+
+
